d2c63688f424e8f142605774a1b3538e273c89ab
Szűcs Gergő <113005538+SzucsGergo1@users.noreply.github.com>
2023-05-17 12:52:38 +0200

    Add files via upload

---
+++ cart/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Ing",
+    price: 5000,
+    desc: "Hétköznapi ing",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Irodai ing",
+    price: 12000,
+    desc: "Prémium minőségű irodai ing",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Póló",
+    price: 3800,
+    desc: "Egyszerű póló",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Öltöny",
+    price: 40000,
+    desc: "Elegáns öltöny",
+    img: "images/img-4.jpg",
+  },
+];
